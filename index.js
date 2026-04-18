828220c9032cb43869484a7deb98ee9c2a2606e6
Bogdan3690 <dblevko@gmail.com>
2026-04-18 13:14:52 -0600

    practice with .race( ) and .all( )

--- index.js
+++ index.js
@@ -16,3 +16,54 @@ promiseNew.then((resultNew) => {console.log(resultNew);
 
  promiseReject.catch((error) => {console.log(error)
  })
+
+
+
+
+
+
+ const horses = [
+  "Secretariat",
+  "Eclipse",
+  "West Australian",
+  "Flying Fox",
+  "Seabiscuit",
+];
+
+const btn = document.querySelector('button')
+
+btn.addEventListener("click", () => {
+console.log("🤖 Заїзд розпочався, ставки не приймаються!")
+
+const horsesPromises = horses.map(horse => run(horse))
+
+Promise.all(horsesPromises).then((result) => {
+    console.log(result);
+    
+    console.log("📝 Заїзд закінчено, приймаються ставки.")
+
+})
+
+Promise.race(horsesPromises).then(({horse, time}) => {
+    console.log(`🎉 Переможець ${horse}, фінишував за ${time}
+часу`)
+});
+})
+
+
+
+
+function run(horse) {
+  return new Promise((resolve) => {
+    const time = getRandomTime(2000, 3500)
+
+    setTimeout(() => {
+        resolve({horse, time})
+
+    }, time);
+  });
+}
+
+function getRandomTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
