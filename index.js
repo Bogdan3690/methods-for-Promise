ed9eaf334c9cd878b5a793fd80ae89f958a7cc20
Bogdan3690 <dblevko@gmail.com>
2026-04-20 21:23:11 -0600

    finish horse race(game)

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const promise = new Promise((resolve, reject) => {
-    resolve(10)
- })
+// const promise = new Promise((resolve, reject) => {
+//     resolve(10)
+//  })
 
- promise.then((result) => {console.log(result);
- })
+//  promise.then((result) => {console.log(result);
+//  })
 
 
 
-const promiseNew = Promise.resolve(20)
+// const promiseNew = Promise.resolve(20)
 
-promiseNew.then((resultNew) => {console.log(resultNew);
- })
+// promiseNew.then((resultNew) => {console.log(resultNew);
+//  })
 
- const promiseReject = Promise.reject('Error 404')
+//  const promiseReject = Promise.reject('Error 404')
 
- promiseReject.catch((error) => {console.log(error)
- })
+//  promiseReject.catch((error) => {console.log(error)
+//  })
 
 
 
@@ -30,25 +30,66 @@ promiseNew.then((resultNew) => {console.log(resultNew);
   "Seabiscuit",
 ];
 
-const btn = document.querySelector('button')
+const btn = document.querySelector('.js-start-race')
+const refs = {
+winnerField: document.querySelector('.js-winner'),
+progressField: document.querySelector('.js-progress'),
+tableBody: document.querySelector('.js-results-table > tbody'),
+};
+
 
 btn.addEventListener("click", () => {
+btn.disabled = true;
+
+refs.progressField.textContent = '🤖 Заїзд розпочався, ставки не приймаються!';
 console.log("🤖 Заїзд розпочався, ставки не приймаються!")
+
+  refs.winnerField.textContent = '';
+  refs.tableBody.innerHTML = '';
 
 const horsesPromises = horses.map(horse => run(horse))
 
-Promise.all(horsesPromises).then((result) => {
-    console.log(result);
-    
-    console.log("📝 Заїзд закінчено, приймаються ставки.")
-
-})
-
 Promise.race(horsesPromises).then(({horse, time}) => {
+    refs.winnerField.textContent =
+      `🎉 Переможець ${horse}, фінішував за ${time} ms`;
     console.log(`🎉 Переможець ${horse}, фінишував за ${time}
 часу`)
 });
+
+Promise.all(horsesPromises).then((results) => {
+    console.table(results);
+    
+    refs.progressField.textContent =
+      '📝 Заїзд закінчено, приймаються ставки.';
+    console.log("📝 Заїзд закінчено, приймаються ставки.")
+
+        const sorted = [...results].sort((a, b) => a.time - b.time);
+
+sorted.forEach(({ horse, time }, index) => {
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${horse}</td>
+    <td>${time}</td>
+  `;
+
+  refs.tableBody.appendChild(tr);
+    });
+    
+    btn.disabled = false;
 })
+  .catch(error => {
+    console.error(error);
+    refs.progressField.textContent = '❌ Помилка';
+    refs.btn.disabled = false;
+  });
+
+})
+
+
+
+
 
 
 
@@ -67,3 +108,5 @@ function run(horse) {
 function getRandomTime(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
